996ee2051ec4428963410da9b24c3301c50acc59
MChuvilevM <mishanyasha324@gmail.com>
2026-07-20 01:34:29 +0300

    feat: implement ApplicationLifetime to define entry point and main window for Avalonia

--- src/GovAdmin.UI/App.axaml.cs
+++ src/GovAdmin.UI/App.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 namespace GovAdmin.UI;
@@ -6,4 +7,14 @@ namespace GovAdmin.UI;
 public partial class App : Application
 {
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
 }
