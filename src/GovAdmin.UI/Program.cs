f1ed298a38351ee8151417de6797ac868ae7e7e3
MChuvilevM <mishanyasha324@gmail.com>
2026-07-19 22:18:15 +0300

    debug: add console output to Program.cs to diagnose startup

--- src/GovAdmin.UI/Program.cs
+++ src/GovAdmin.UI/Program.cs
@@ -6,8 +6,11 @@ namespace GovAdmin.UI;
 internal class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Debug: Main started");
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
